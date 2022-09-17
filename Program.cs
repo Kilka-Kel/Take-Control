fc4096e131d3f7760cdd95c1e2b73b5ae581274e
Kilka <Keilija@mail.ru>
2022-09-17 17:34:16 +0300

    задаём массив и выводим его

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Сначала выводим наш массив");
+
+
+
+string [] Str= { "Mon", "Tuesd", "Wednesday", "Thirsday", "Friday", "Saturday","Sun" };
+
+
+void PrintStr(string[] coll)//печатаем массив
+{
+    int lenghtPrint = coll.Length;
+    int indexPrint = 0;
+    while (indexPrint < lenghtPrint)
+    {
+        System.Console.Write($"{coll[indexPrint]} ");
+        indexPrint++;
+    }
+        System.Console.WriteLine();
+}
+
+PrintStr(Str);
+
